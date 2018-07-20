e2bb481b5a39eb52f27c7706bae34f27b0135117
chemot <40890269+chemot@users.noreply.github.com>
2018-07-20 23:31:08 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.18.0"
-  LA = "CHROMEOS\t7.18.0\tiVipro\t11.12.1"
+  UA = "Line/1.4.17"
+  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 #  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 #  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
 
